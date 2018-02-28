288947af3099f0db858c88b5d0a646020f6370f4
Noah Wotring <noah13wotring@outlook.com>
2018-02-28 12:03:41 -0500

    create_profile_with_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 require 'bcrypt'
 class User < ApplicationRecord
+  has_one :profile
+  after_create :create_user_profile
+
   serialize :interests, Array
   has_many :direct_messages
   validates :username, presence: true
   validates :password, length: {minimum: 8}
   has_secure_password
+
+
+
+  def create_user_profile
+    Profile.create(user_id: self.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228062722) do
+ActiveRecord::Schema.define(version: 20180228165327) do
 
   create_table "direct_messages", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180228062722) do
     t.text "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
