cb0781b8129f44be3e09b4d9e0f8d1b2bbaebc6a
kudubaba <kudubaba@gmail.com>
2018-03-01 14:08:58 -0500

    Added simple messaging between users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 require 'bcrypt'
 class User < ApplicationRecord
   serialize :interests, Array
-  has_many :direct_messages
   validates :username, presence: true
   validates :password, length: {minimum: 8}
   has_secure_password
+  has_many :conversations, :foreign_key => :sender_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :conversations do
+    resources :messages
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
